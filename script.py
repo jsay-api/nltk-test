37dfbef2f9b31c61140f04a0de26f1fe8fb5aac3
jsay-api <julia.sayapina@me.com>
2016-09-11 13:15:49 +0300

    First task completed

--- script.py
+++ script.py
@@ -6,67 +6,74 @@
 
 The script parces the files from the folder of user choice and writes id, title, text with type "UNPROC" 
 (takes place if not <body></body>) and body if exists into the new files. 
-Requires Python3, bs4, NLTK. nltk.download() should be run before the script execution.
+Requires Python3, bs4, NLTK. nltk.download() should be run before the script execution. 
+Please check if there are no meta files in folder with corpus files (e.g., .DS_Stores).
+Usage: python script.py [--topwords] folderpath
 
 """
 
-from bs4 import BeautifulSoup
 import nltk
+import sys
+from os import listdir
+from os.path import isfile, join
+from bs4 import BeautifulSoup
 from nltk import sent_tokenize, word_tokenize, pos_tag
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 from itertools import zip_longest
 # nltk.download()
 
 
-folder = 'articles/'
-# file = 'reut2-000.sgm'
+def process(tag, file):
+    """Processes tag content and writes it into the file"""
+    if tag:
+        tagval = tag.text
+        if tag.name == 'title':
+            file.write('\t\t<subject>'+tagval+'</subject>\n')
+        else:
+            sents = sent_tokenize(tagval)
+            for s in sents:
+                file.write("\t\t<sentence>"+(s.replace('\n', ''))+"</sentence>\n")
 
-for file in folder:
-    filename = file.split('.')[0]
 
-    def process(tag):
-        """Processes tag content and writes it into the file"""
-        if tag:
-            tagval = tag.text
-            if tag.name == 'title':
-                newfile.write('\t\t<subject>'+tagval+'</subject>\n')
-            else:
-                sents = sent_tokenize(tagval)
-                for s in sents:
-                    newfile.write("\t\t<sentence>"+(s.replace('\n', ''))+"</sentence>\n")
-            
-            
+def make_xml(folderpath):
+    onlyfiles = [join(folderpath, f) for f in listdir(folderpath) if (isfile(join(folderpath, f)) and (join(folderpath, f)).endswith('.sgm'))]
 
-    with open(file) as oldfile, open('output_'+filename+'.xml', 'w') as newfile:
-        soup = BeautifulSoup(oldfile, 'html.parser')
+    for file in onlyfiles:
+        filename = file.split('.')[0].split('/')[1]
 
-        newfile.write('<articles>\n')
-        for reuter in soup.find_all('reuters'):
-            idval = reuter['newid']
-            newfile.write('\n'+'\t<article id=”'+idval+'">\n')
-            for title, text, body in zip_longest(reuter.find_all('title'), reuter.find_all('text', {'type' : 'UNPROC'}), reuter.find_all('body')):
-                for instance in [title, text, body]: process(instance)
-            newfile.write('\t</article>\n')
-        newfile.write('</articles>')
+        with open(file) as oldfile, open(join(folderpath, 'output_'+filename+'.xml'), 'w') as newfile:
+            soup = BeautifulSoup(oldfile, 'html.parser')
 
+            newfile.write('<articles>\n')
+            for reuter in soup.find_all('reuters'):
+                idval = reuter['newid']
+                newfile.write('\n'+'\t<article id=”'+idval+'">\n')
+                for title, text, body in zip_longest(reuter.find_all('title'), reuter.find_all('text', {'type' : 'UNPROC'}), reuter.find_all('body')):
+                    for instance in [title, text, body]: process(instance, newfile)
+                newfile.write('\t</article>\n')
+            newfile.write('</articles>')
 
 
 
+def top_words(folderpath):
+    pass
 
 
-# def main():
-# 	if len(sys.argv) != 2:
-#         print('usage: python script.py {--topwords} folder')
-#         sys.exit(1)
+def main():
+    args = sys.argv[1:]
 
-#     option = sys.argv[1]
-#     corpus = sys.argv[2]
-#     if option == '--count':
-#         print_words(corpus)
-#     # else:
-#     #     print('unknown option: ' + option)
-#     sys.exit(1)
+    if not args:
+        print('usage: python script.py folderpath [--topwords]')
+        sys.exit(1)
+
+    folderpath = args[0]
+    make_xml(folderpath)
+
+    if len(args) == 2: 
+        top_words(folderpath)
+
+    sys.exit(1)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
